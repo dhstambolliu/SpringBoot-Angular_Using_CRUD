5b5be030e1ff7f3a38761f0f21d589d065afb694
dhstambolliu <stambolliudhimiter@gmail.com>
2022-04-25 17:29:58 +0200

    backend + fronend simple interface using crud

---
+++ backend/employeemanager/src/main/java/dhstambolliu/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package dhstambolliu.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
